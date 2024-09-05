77512fd37df8d55747b1079f186ca4aa8543c359
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-09-05 10:09:39 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller {
+    public function user($id, $nama) {
+        return view('blog.user')
+        -> with('id', $id)
+        -> with('nama', $nama);
+    }
+}
---
+++ resources/views/blog/babykid.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daftar Product</title>
+</head>
+<body>
+    <h1>Ini adalah daftar produk category {{$category}}</h1>
+    <p>Berikut adalah daftar product {{$category}}</p>
+    <li>Popok</li>
+    <li>Susu</li>
+    <li>Bubur Bayi</li>
+</body>
+</html>
